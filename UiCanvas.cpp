2c835766e2c7a442984e4291b3cb9cff94e51325
김혜인 <rjqnrdl7524@naver.com>
2025-07-21 09:27:56 +0900

    최종본

--- UiCanvas.cpp
+++ UiCanvas.cpp
@@ -163,7 +163,6 @@ void UiCanvas::ClickButton()
 				&& (btn->GetGlobalBounds().top <= mousePos.y && mousePos.y <= btn->GetGlobalBounds().top + btn->GetGlobalBounds().height))
 			{
 				btn->OnClick();
-				SOUND_MGR.PlaySfx("sound/click.ogg", false);
 				break;
 			}
 		}
